5459b0cd909a5e836e710c8800f04b597dab9205
marcu <marcussoh38@gmail.com>
2023-05-20 16:43:49 +0800

    fixed incorrect calendar day color

--- src/components/Calendar/CalendarDay.tsx
+++ src/components/Calendar/CalendarDay.tsx
@@ -7,6 +7,7 @@ import { dateEncoder, dateParser } from "./CalendarContainer";
 
 // Color values
 const DARK__UNSELECTABLE_TEXT_COLOR = "gray.600";
+const DARK__UNSELECTABLE_CURRENT_DAY_TEXT_COLOR = "gray.500";
 const LIGHT__UNSELECTABLE_TEXT_COLOR = "gray.400";
 const DARK__SELECTED_DATE_COLOR = "blue.800";
 const LIGHT__SELECTED_DATE_COLOR = "blue.200";
@@ -23,10 +24,10 @@ type CalendarDayProps = {
     selected: boolean;
     prevSelected: boolean;
     nextSelected: boolean;
-    onTouchEnd: (e: React.TouchEvent<HTMLDivElement>, dateStr: string) => void
-    startDate: Date, // the start date of the calendar. Defaults to today
-    endDate: Date // the end date of the calendar. Defaults to 1 year from today
-    allowedDates?: string[]
+    onTouchEnd: (e: React.TouchEvent<HTMLDivElement>, dateStr: string) => void;
+    startDate: Date; // the start date of the calendar. Defaults to today
+    endDate: Date; // the end date of the calendar. Defaults to 1 year from today
+    allowedDates?: string[];
     // type: 0|1|2|3|4 // 0: unselected, 1: circle, 2: square, 3: left square, 4: right square
 };
 
@@ -45,12 +46,16 @@ const CalendarDay = ({
     onTouchEnd,
     startDate,
     endDate,
-    allowedDates
+    allowedDates,
 }: CalendarDayProps) => {
     // console.log("day rerender")
     const UNSELECTABLE_TEXT_COLOR = useColorModeValue(
         LIGHT__UNSELECTABLE_TEXT_COLOR,
         DARK__UNSELECTABLE_TEXT_COLOR
+    );
+    const UNSELECTABLE_CURRENT_DAY_TEXT_COLOR = useColorModeValue(
+        LIGHT__UNSELECTABLE_TEXT_COLOR,
+        DARK__UNSELECTABLE_CURRENT_DAY_TEXT_COLOR
     );
     const SELECTED_DATE_COLOR = useColorModeValue(
         LIGHT__SELECTED_DATE_COLOR,
@@ -58,7 +63,10 @@ const CalendarDay = ({
     );
     const CURRENT_DATE_COLOR = useColorModeValue("gray.200", "gray.600");
 
-    const isSelectable = isAfter(dateParser(dataKey), subDays(startDate, 1)) && isBefore(dateParser(dataKey), addDays(endDate, 1)) && (allowedDates?.includes(dataKey) || !allowedDates);
+    const isSelectable =
+        isAfter(dateParser(dataKey), subDays(startDate, 1)) &&
+        isBefore(dateParser(dataKey), addDays(endDate, 1)) &&
+        (allowedDates?.includes(dataKey) || !allowedDates);
 
     /**
      * Calculates the color of the circle around the date, depending on whether
@@ -98,22 +106,26 @@ const CalendarDay = ({
      */
     const getTextColor = () => {
         if (!isSelectable) {
-            return UNSELECTABLE_TEXT_COLOR;
+            if (isSameDay(dateParser(dataKey), new Date())) {
+                return UNSELECTABLE_CURRENT_DAY_TEXT_COLOR;
+            } else {
+                return UNSELECTABLE_TEXT_COLOR;
+            }
         } else {
             return "unset";
         }
     };
 
     /**
-     * 
+     *
      * @returns the border radius of the day depending on the selected state
      */
     const getRadius = () => {
         if (!selected) {
-            return "99999px"
+            return "99999px";
         }
         if (prevSelected && nextSelected) {
-            return "0"
+            return "0";
         }
         if (!prevSelected && nextSelected) {
             return "99999px 0 0 99999px";
@@ -122,24 +134,30 @@ const CalendarDay = ({
             return "0 99999px 99999px 0";
         }
         if (!prevSelected && !nextSelected) {
-            return "99999px"
+            return "99999px";
         }
-    }
+    };
 
     const getWidth = () => {
         if (selected && (nextSelected || prevSelected)) {
-            return "100%"
+            return "100%";
         } else {
-            return "unset"
+            return "unset";
         }
-    }
+    };
 
     const prevDateStr = dateEncoder(subDays(dateParser(dataKey), 1));
     const nextDateStr = dateEncoder(addDays(dateParser(dataKey), 1));
 
     return (
         <Box data-key={dataKey} className={getClassName()} width="100%">
-            <Square bg={getBgColor()} size="36px" mx="auto" borderRadius={getRadius()} minW={getWidth()}  >
+            <Square
+                bg={getBgColor()}
+                size="36px"
+                mx="auto"
+                borderRadius={getRadius()}
+                minW={getWidth()}
+            >
                 <Text {...BODY_STYLES} color={getTextColor()}>
                     {children}{" "}
                 </Text>
