b2441914d2054bfb46ddc5054c870145f51fca05
marcu <marcussoh38@gmail.com>
2023-06-12 15:58:31 +0800

    add color

--- src/components/Buttons/NavigationButtons.tsx
+++ src/components/Buttons/NavigationButtons.tsx
@@ -1,27 +1,46 @@
-import { IconButton } from "@chakra-ui/react";
+import { IconButton, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { useTelegram } from "../../context/TelegramProvider";
 
 export const GoLeftButton = React.memo(
-    ({ canGoLeft, goLeft }: { canGoLeft: boolean; goLeft: () => void }) => (
-        <IconButton
-            size="xs"
-            isDisabled={!canGoLeft}
-            onClick={goLeft}
-            aria-label="Previous month"
-            icon={<FaChevronLeft />}
-        />
-    )
+    ({ canGoLeft, goLeft }: { canGoLeft: boolean; goLeft: () => void }) => {
+        const { style } = useTelegram();
+        const _btnColor = useColorModeValue("purple.200", "purple.700");
+        const _disabledBtnColor = useColorModeValue("#EDF2F7", "#1A202C");
+
+        const btnColor = style?.button_color || _btnColor;
+
+        return (
+            <IconButton
+                size="xs"
+                isDisabled={!canGoLeft}
+                onClick={goLeft}
+                aria-label="Previous month"
+                icon={<FaChevronLeft />}
+                // colorScheme="gray"
+                color={btnColor}
+            />
+        );
+    }
 );
 
 export const GoRightButton = React.memo(
-    ({ canGoRight, goRight }: { canGoRight: boolean; goRight: () => void }) => (
-        <IconButton
-            size="xs"
-            isDisabled={!canGoRight}
-            onClick={goRight}
-            aria-label="Next month"
-            icon={<FaChevronRight />}
-        />
-    )
+    ({ canGoRight, goRight }: { canGoRight: boolean; goRight: () => void }) => {
+        const { style } = useTelegram();
+        const _btnColor = useColorModeValue("purple.200", "purple.700");
+
+        const btnColor = style?.button_color || _btnColor;
+
+        return (
+            <IconButton
+                size="xs"
+                isDisabled={!canGoRight}
+                onClick={goRight}
+                aria-label="Next month"
+                icon={<FaChevronRight />}
+                color={btnColor}
+            />
+        );
+    }
 );
