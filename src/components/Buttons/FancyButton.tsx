8e15f1a17c493b91cdcbaf838f0abac1635e6cd2
marcu <marcussoh38@gmail.com>
2023-06-12 15:19:34 +0800

    update fancybutton

--- src/components/Buttons/FancyButton.tsx
+++ src/components/Buttons/FancyButton.tsx
@@ -1,5 +1,5 @@
 import { Button, ButtonProps } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 
 const FancyButton = ({
     children,
@@ -8,13 +8,26 @@ const FancyButton = ({
     children: React.ReactNode | React.ReactNode[];
     props?: ButtonProps;
 }) => {
+    // on hover, min background pos: 50, max background pos: 100
+    const [x, setX] = useState<number>(150);
+    const onMouseMove = (
+        e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+        setX(e.nativeEvent.offsetX > 300 ? 300 : e.nativeEvent.offsetX);
+    };
+
+    const xpercent = ((x / 300) * 100) / 2 + 50;
+
     return (
         <Button
+            onMouseMove={onMouseMove}
             rounded={"full"}
             _hover={{
                 // marginLeft: "65px",
 
-                backgroundPosition: !props?.isDisabled ? "100% 0px" : "unset",
+                backgroundPosition: !props?.isDisabled
+                    ? `${xpercent}% 0px`
+                    : "unset",
             }}
             size="lg"
             sx={{
@@ -26,7 +39,7 @@ const FancyButton = ({
                     "linear-gradient(90deg, rgba(255,87,87,1) 0%, rgba(140,82,255,1) 100%)",
                 boxShadow: "rgb(140, 82, 255, 0.65) 0 4px 15px 0px",
                 transition:
-                    "all 0.3s ease-in-out 0s, background 0.3s ease-in-out 0s, transform 0.1s ease-out 0s",
+                    "all 0.3s ease-in-out 0s, background 0.05s ease-in-out 0s, transform 0.1s ease-out 0s",
             }}
             _focus={{
                 boxShadow: !props?.isDisabled
@@ -44,4 +57,4 @@ const FancyButton = ({
     );
 };
 
-export default FancyButton;
+export default React.memo(FancyButton);
