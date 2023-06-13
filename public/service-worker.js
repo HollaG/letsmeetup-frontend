a541db20d7a70576bf854bc3c7546e25d0c26b4b
marcu <marcussoh38@gmail.com>
2023-06-13 12:57:38 +0800

    added serviceworker

---
+++ public/service-worker.js
@@ -0,0 +1,65 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker.register("sw.js").then(
+            function (registration) {
+                // Registration was successful
+                console.log(
+                    "ServiceWorker registration successful with scope: ",
+                    registration.scope
+                );
+            },
+            function (err) {
+                // registration failed :(
+                console.log("ServiceWorker registration failed: ", err);
+            }
+        );
+    });
+}
+
+let CACHE_NAME = "look4times-cache";
+const urlsToCache = [
+    "/",
+    "/create",
+    "/policies/privacy",
+    "/policies/terms",
+    "/about",
+];
+self.addEventListener("install", function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function (event) {
+    if (!event.request.url.match(/http/i)) return;
+    event.respondWith(
+        caches.match(event.request).then((cacheRes) => {
+            return (
+                cacheRes ||
+                fetch(event.request).then((fetchRes) => {
+                    return caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request.url, fetchRes.clone());
+                        return fetchRes;
+                    });
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    // Delete old versions of the cache
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys
+                    .filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            );
+        })
+    );
+});
